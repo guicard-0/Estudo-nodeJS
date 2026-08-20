7558f86806373775505110a83f4c8aabf0788873
guicard-0 <guiz0206@gmail.com>
2026-08-20 17:45:52 -0300

    Add files via upload

--- 04-commonjs-modules/src/main.js
+++ 04-commonjs-modules/src/main.js
@@ -1,11 +1,14 @@
 const p = require("./Services/products")
+const test = require("./Services/config")
 
 async function main() {
     console.log("Carrinho:"); 
     
-    p.getFullname("408", "mousepad");
-    p.getFullname("410", "mouse");
-    p.getProductLabel("mousepad");
+    //p.getFullname("408", "mousepad");
+    //p.getFullname("410", "mouse");
+    //p.getProductLabel("mousepad");
+
+    console.log(config.);
 }
 
 main();
